aa3f9261b58e9d6c6b9fe76e542b3f60cb2f44f2
Alef <alef.carlos@gmail.com>
2022-01-26 11:12:11 -0300

    feat: bump versions feat: dotnet new install

---
+++ plugins/scaffolder-dotnet-backend/src/actions/dotnettemplate-install.ts
@@ -0,0 +1,36 @@
+import { createTemplateAction, runCommand } from '@backstage/plugin-scaffolder-backend';
+
+export const dotnetNewAction = () => {
+  return createTemplateAction<{ package: string, args: string[], targetPath?: string }>({
+    id: 'dotnet:new:install',
+    description: 'Install a template package using dotnet new command',
+    schema: {
+      input: {
+        required: ['template'],
+        type: 'object',
+        properties: {
+          package: {
+            type: 'string',
+            title: 'Package name'
+          },
+          args: {
+            type: 'array',
+            items: {
+              type: 'string',
+            },
+            title: 'Arguments to pass to the command'
+          }
+        },
+      },
+    },
+    async handler(ctx) {
+      await runCommand({
+        command: 'dotnet',
+        args: ['--install', ctx.input.package, ...ctx.input.args],
+        logStream: ctx.logStream
+      });
+
+      ctx.logger.info(`Package ${ctx.input.package} installed`);
+    },
+  });
+};
