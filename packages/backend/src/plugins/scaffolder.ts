4b1b228f7f7fe791c00df91b41bf7c72467d7cb6
Phill Morton <me@phillmorton.co.uk>
2023-03-23 23:12:36 +0000

    integrate plugin to root backstage instance for easy dev.

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,20 +1,41 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import { createRouter, createBuiltinActions } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
+
+
+import { dotnetNewAction, dotnetBuildAction, dotnetInstallTemplateAction } from '../../../../plugins/plugin-scaffolder-dotnet-backend'
+import { ScmIntegrations } from '@backstage/integration';
+
+
+
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
-  const catalogClient = new CatalogClient({
-    discoveryApi: env.discovery,
-  });
+const catalogClient = new CatalogClient({
+  discoveryApi: env.discovery,
+});
+const integrations = ScmIntegrations.fromConfig(env.config);
 
-  return await createRouter({
-    logger: env.logger,
-    config: env.config,
-    database: env.database,
-    reader: env.reader,
-    catalogClient,
-  });
+
+const builtInActions = createBuiltinActions({
+  integrations,
+  catalogClient,
+  config: env.config,
+  reader: env.reader,
+});
+
+const actions = [...builtInActions, dotnetNewAction(), dotnetBuildAction(), dotnetInstallTemplateAction()];
+
+
+
+return await createRouter({
+  actions: actions,
+  logger: env.logger,
+  config: env.config,
+  database: env.database,
+  reader: env.reader,
+  catalogClient
+});
 }
